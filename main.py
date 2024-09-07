6c56b3954781ae235d317c13fb4b0872a8265bce
8-bit-confusion <lilywasko@gmail.com>
2024-09-07 13:09:58 -0400

    Update main to use GalleryScrape and ChatUtils

--- main.py
+++ main.py
@@ -1,64 +1,17 @@
-import requests
-import os
-from dotenv import load_dotenv
-from openai import OpenAI
-import base64
+from gallery_scrape import Gallery
+from chat_utils import Prompts, ChatClient
 
-load_dotenv()
+if __name__ == "__main__":
+    client = ChatClient()
+    iunsct_gallery = Gallery.from_csv("data/iunsct.csv")
 
-OpenAI.api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+    for base64_image in iunsct_gallery.values():
+        response = client.response(
+            Prompts.image_prompt(
+                "please list the clothing items in this image. \
+                be detailed in your description of garment styles, fabrics, textures, accessories, etc.",
+                base64_image
+            )
+        )
 
-def encode_image(image_path):
-  with open(image_path, 'rb') as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-
-def download_image(url, filename):
-  response = requests.get(url)
-  with open(filename, 'wb') as f:
-    f.write(response.content)
-
-def gen_image_urls(csv_filename):
-    with open(csv_filename, 'r') as csv:
-        return [str(line[:-1]).replace("\x00", "") for line in csv.readlines()]
-
-def encode_from_url(url):
-   response = requests.get(url)
-   return base64.b64encode(response.content).decode('utf-8')
-
-REM_CHARS = "\x00\xfe\xff"
-def clean_url(url):
-    for c in REM_CHARS:
-        url = url.replace(c, "")
-    return url
-
-responses = []
-for url in gen_image_urls("iunsct.csv"):
-    if url == "": continue
-    base64_image = encode_from_url(clean_url(url))
-
-    response = client.chat.completions.create(
-        model = "gpt-4o",
-        messages = [
-            {
-                'role': "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "please list the clothing items in this image. \
-                            be detailed in your description of garment styles, fabrics, textures, accessories, etc."
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": f"data:image/jpeg;base64, {base64_image}"
-                        }
-                    }
-                ]
-            }
-        ],
-        temperature = 0.7,
-    )
-
-    print(response.choices[0].message.content)
-    responses.append(response.choices[0].message.content)
+        print(response.choices[0].message.content)
