8e989189882602d27e33f957d7487b040b6b063a
Nicol <nicchbeir@gmail.com>
2024-09-30 18:30:07 -0400

    WE ARE SO COOL (Lily is cool, she fixed it)

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ from chat_utils import Prompts, ChatClient
 if __name__ == "__main__":
     client = ChatClient()
     # https://in.pinterest.com/iunsct/coquette-outfits/
-    iunsct_gallery = Gallery.from_csv("data/iunsct.csv")
+    #iunsct_gallery = Gallery.from_csv("data/iunsct.csv")   
 
-    for base64_image in iunsct_gallery.values():
+
+    gallery = Gallery()
+
+    urls = ["https://i.pinimg.com/originals/9e/62/61/9e626127c8161e999d6b03dd67090e85.jpg"]   
+
+    for url in urls:
+        gallery.add_from_image_link(url)
+
+    for base64_image in gallery.values():
         response = client.response(
             Prompts.image_prompt(
-                "please list the clothing items in this image. \
-                be detailed in your description of garment styles, fabrics, textures, accessories, etc.",
+                "Given the following image(s), provide objective descriptors, \
+                each separated by commas, that describe solely the outfit. \
+                Do not describe the person’s body type, skin color, or any other \
+                attributes unrelated to the clothes. Include details on fabric types, \
+                garment fit, cuts of the neckline, sleeves, and bottoms, as well as any accessories. \
+                Additionally, based on the descriptors, suggest an aesthetic style label that best \
+                fits the outfit’s overall look. Each descriptor can be no more than three words",
                 base64_image
             )
         )
